cfda6267fc7dc7585e406e6b963bb3384909c054
Daniel <inad9300@gmail.com>
2017-10-22 10:28:42 +0200

    Change comment for consistency and clarity

--- src/testing/support/createElement.ts
+++ src/testing/support/createElement.ts
@@ -1,7 +1,7 @@
 /**
  * Create HTML and SVG elements from a valid HTML string.
  *
- * The given HTML must contain only one root element, of type `Element`.
+ * NOTE The given HTML must contain only one root element, of type `Element`.
  */
 export function createElement(html: string): Element {
     return document.createRange().createContextualFragment(html).firstElementChild as Element
