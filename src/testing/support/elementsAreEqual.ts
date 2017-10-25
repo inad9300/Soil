662e7cb8f3424a6b915d3320935c50ac2d3ebd8e
Daniel <inad9300@gmail.com>
2017-10-25 21:17:57 +0200

    Improve arguments naming

--- src/testing/support/elementsAreEqual.ts
+++ src/testing/support/elementsAreEqual.ts
@@ -3,9 +3,9 @@ const {assert} = intern.getPlugin('chai')
 /**
  * Check if two HTML or SVG elements are exactly equal.
  */
-export function elementsAreEqual(e1: Element, e2: Element): void {
-    assert.notEqual(e1, e2, `trying to test if an element is equal to itself (tag <${e1.tagName}>)`)
-    assert.isNotEmpty(e1.outerHTML, `the HTML of the created element is empty (tag <${e1.tagName}>)`)
-    assert.strictEqual(e1.outerHTML, e2.outerHTML, `the HTML of the two elements created is different (tag <${e1.tagName}>)`)
-    assert.isTrue(e1.isEqualNode(e2), `elements are not deeply equal (tag <${e1.tagName}>)`)
+export function elementsAreEqual(actual: Element, expected: Element): void {
+    assert.notEqual(actual, expected, `trying to test if an element is equal to itself (tag <${actual.tagName}>)`)
+    assert.isNotEmpty(actual.outerHTML, `the HTML of the created element is empty (tag <${actual.tagName}>)`)
+    assert.strictEqual(actual.outerHTML, expected.outerHTML, `the HTML of the two elements created is different (tag <${actual.tagName}>)`)
+    assert.isTrue(actual.isEqualNode(expected), `elements are not deeply equal (tag <${actual.tagName}>)`)
 }
