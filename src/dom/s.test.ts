608e73250b4fa3dce7f30e82730e9c26cb1f5f09
Daniel <inad9300@gmail.com>
2017-10-25 21:25:29 +0200

    Initial file for s() tests

---
+++ src/dom/s.test.ts
@@ -0,0 +1,11 @@
+const {suite, test} = intern.getInterface('tdd')
+const {assert} = intern.getPlugin('chai')
+import {createElement} from '../testing/support/createElement'
+import {elementsAreEqual} from '../testing/support/elementsAreEqual'
+
+import {s, SVGElementProperties} from './s'
+
+suite('s()', () => {
+
+    // TODO
+})
