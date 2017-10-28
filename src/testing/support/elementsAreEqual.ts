9c1999acf0b4b9e0d4a427d050e33c4d2fb7cfbb
Daniel <inad9300@gmail.com>
2017-10-28 12:48:30 +0200

    Allow skipping native deep equality check

--- src/testing/support/elementsAreEqual.ts
+++ src/testing/support/elementsAreEqual.ts
@@ -3,9 +3,11 @@ const {assert} = intern.getPlugin('chai')
 /**
  * Check if two HTML or SVG elements are exactly equal.
  */
-export function elementsAreEqual(actual: Element, expected: Element): void {
+export function elementsAreEqual(actual: Element, expected: Element, skipDeepEquality = false): void {
     assert.notEqual(actual, expected, `trying to test if an element is equal to itself (tag <${actual.tagName}>)`)
     assert.isNotEmpty(actual.outerHTML, `the HTML of the created element is empty (tag <${actual.tagName}>)`)
     assert.strictEqual(actual.outerHTML, expected.outerHTML, `the HTML of the two elements created is different (tag <${actual.tagName}>)`)
-    assert.isTrue(actual.isEqualNode(expected), `elements are not deeply equal (tag <${actual.tagName}>)`)
+    if (!skipDeepEquality) {
+        assert.isTrue(actual.isEqualNode(expected), `elements are not deeply equal (tag <${actual.tagName}>)`)
+    }
 }
