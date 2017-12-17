55db5441b0d073bf1abe30321dd646e588c23204
Daniel <inad9300@gmail.com>
2017-12-17 11:22:13 +0100

    Add comment to DeepPartial type

--- src/extra/DeepPartial.ts
+++ src/extra/DeepPartial.ts
@@ -1,5 +1,8 @@
 /**
  * Version of TypeScript's `Partial` type that considers nested properties as optional too, recursively.
+ *
+ * FIXME This type undermines type safety, e.g. the compiler will not check function signatures. An eye must be kept on
+ * https://github.com/Microsoft/TypeScript/issues/12424.
  */
 export type DeepPartial<T> = {
     [P in keyof T]?: T[P] | DeepPartial<T[P]>
