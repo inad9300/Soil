d04629b9ebc626100718255aafec0627ad7d1d4c
Daniel <inad9300@gmail.com>
2018-01-21 16:40:58 +0100

    Expose extend() function through index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ export {Component} from './core/Component'
 export {ComponentFactory} from './core/ComponentFactory'
 export {Channel} from './core/Channel'
 export {assert} from './core/assert'
+export {extend} from './core/extend'
 
 import * as h from './dom/h'
 import * as s from './dom/s'
