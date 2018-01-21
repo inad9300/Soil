1ec30d8f761eef57760bf29f64225094c3d76d7a
Daniel <inad9300@gmail.com>
2018-01-21 14:07:27 +0100

    Add extend() function

---
+++ src/core/extend.ts
@@ -0,0 +1,13 @@
+/**
+ * Extend `target` with the enumerable own properties in `source`. Similar to `Object.assign()`, but including getters
+ * and setters.
+ */
+export function extend<T, U>(target: T, source: U): T & U {
+    for (const prop in source) {
+        if (source.hasOwnProperty(prop)) {
+            Object.defineProperty(target, prop,
+                Object.getOwnPropertyDescriptor(source, prop) as PropertyDescriptor)
+        }
+    }
+    return target as T & U
+}
