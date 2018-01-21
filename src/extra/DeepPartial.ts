0ef821d0c9be3170dd8094bb5a4dc78e2111b301
Daniel <inad9300@gmail.com>
2018-01-21 14:07:53 +0100

    Add link to alternative implementation

--- src/extra/DeepPartial.ts
+++ src/extra/DeepPartial.ts
@@ -3,6 +3,8 @@
  *
  * FIXME This type undermines type safety, e.g. the compiler will not check function signatures. An eye must be kept on
  * https://github.com/Microsoft/TypeScript/issues/12424.
+ *
+ * TODO Consider the alternative proposed in https://github.com/Microsoft/TypeScript/issues/12424.
  */
 export type DeepPartial<T> = {
     [P in keyof T]?: T[P] | DeepPartial<T[P]>
