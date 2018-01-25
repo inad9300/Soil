a148f5a8e1c2f004e54d0af926c29c471ecf2b04
Daniel <inad9300@gmail.com>
2018-01-25 21:45:21 +0100

    Improve DeepPartial type

--- src/extra/DeepPartial.ts
+++ src/extra/DeepPartial.ts
@@ -1,11 +1,23 @@
+export type False = '0'
+export type True = '1'
+
+export type If<C extends True | False, Then, Else> = {'0': Else, '1': Then}[C]
+
+export type Diff<T extends string, U extends string> = (
+    {[P in T]: P} & {[P in U]: never} & {[x: string]: never}
+)[T]
+
+export type X<T> = Diff<keyof T, keyof Object>
+
+export type Is<T, U> = (Record<X<T & U>, False> & Record<any, True>)[Diff<X<T>, X<U>>]
+
 /**
  * Version of TypeScript's `Partial` type that considers nested properties as optional too, recursively.
  *
- * FIXME This type undermines type safety, e.g. the compiler will not check function signatures. An eye must be kept on
- * https://github.com/Microsoft/TypeScript/issues/12424.
- *
- * TODO Consider the alternative proposed in https://github.com/Microsoft/TypeScript/issues/12424.
+ * FIXME The current implementation is taken from https://github.com/Microsoft/TypeScript/issues/12424 and has at least
+ * one known issue. Newer versions of TypeScript will probably allow to create a more robust and more readable version
+ * of this type.
  */
 export type DeepPartial<T> = {
-    [P in keyof T]?: T[P] | DeepPartial<T[P]>
+    [P in keyof T]?: If<Is<T[P], object>, T[P], DeepPartial<T[P]>>
 }
