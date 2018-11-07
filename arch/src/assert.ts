52afce97babd438b7ae8816015ef6b7dd766e1dc
inad9300 <inad9300@gmail.com>
2018-11-07 21:00:11 +0100

    Minor adjustment

--- arch/src/assert.ts
+++ arch/src/assert.ts
@@ -29,7 +29,7 @@ export function assert(assertion: boolean | (() => boolean), message: string = '
     if (!ok) {
         if (assertionIsFunction) {
             if (message) {
-                message += '\n\t'
+                message += ' // '
             }
             message += 'Assertion was: ' + assertion.toString()
         }
