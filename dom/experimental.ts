103b0c54a01804ac3839c8dffdd73d39968af501
inad9300 <inad9300@gmail.com>
2018-12-15 15:52:43 +0100

    Progress programatically defining properties for element creation

--- dom/experimental.ts
+++ dom/experimental.ts
@@ -1,8 +1,4 @@
-// until(), repeat(), etc. "directives"
-
-type Nil = null | undefined
-
-type Primitive = boolean | number | string | symbol | Nil
+type Primitive = boolean | number | string | symbol | null | undefined
 
 type IfEquals<X, Y, Then, Else> =
     (<T> () => T extends X ? 1 : 2) extends
@@ -10,133 +6,73 @@ type IfEquals<X, Y, Then, Else> =
             ? Then
             : Else
 
-type DeepPartial<T> = {
-    [P in keyof T]?: T[P] extends never
+type Clean<T> = Pick<T, {
+    [P in keyof T]: string extends P
         ? never
-        : T[P] extends Array<infer U>
-            ? Array<DeepPartial<U>>
-            : T[P] extends ReadonlyArray<infer U>
-                ? ReadonlyArray<DeepPartial<U>>
-                : T[P] extends Primitive | Function
-                    ? T[P]
-                    : DeepPartial<T[P]>
-}
-
-type PickDefined<O> = Pick<O, {
-    [K in keyof O]: O[K] extends undefined ? never : K /* IfEquals<O[K], {}, never, K> */
-}[keyof O]>
-
-// FIXME
-type DeepPickDefined<T> = PickDefined<{
-    [P in keyof T]: /* T[P] extends Array<infer U>
-        ? Array<DeepPickDefined<U>>
-        : T[P] extends ReadonlyArray<infer U>
-            ? ReadonlyArray<DeepPickDefined<U>>
-            : */ T[P] extends Primitive | Function
-                ? T[P]
-                : DeepPickDefined<T[P]>
-}>
-
-type DPD = DeepPickDefined<{
-    n?: never
-    u?: undefined
-    b?: boolean
-    o?: {
-        n?: never
-        u?: undefined
-        b?: boolean
-        o?: {}
-    }
-}>
-let dpd: DPD = {
-    // n: undefined!,
-    b: false,
-    // u: undefined,
-    o: {
-        // n: undefined!,
-        b: false,
-        // u: undefined,
-        // WAT: 666
-    }
-}
-
-type KnownKeys<T> = {
-    [K in keyof T]: string extends K
-        ? never
-        : number extends K
+        : number extends P
             ? never
-            : K
+            : T[P] extends never
+                ? never
+                : IfEquals<T[P], {}, never, P>
 } extends {[_ in keyof T]: infer U}
     ? U
-    : never
+    : never>
 
-// FIXME `{[k: string]: number, n: number}` becomes `{[k: string]: never, n: number}`, which is invalid (results in `{}`).
-// type WithoutIndices<T> = {
-//     [P in keyof T]: T[P] extends never
-//         ? never
-//         : P extends KnownKeys<T>
-//             ? T[P] extends Array<infer U>
-//                 ? Array<WithoutIndices<U>>
-//                 : T[P] extends ReadonlyArray<infer U>
-//                     ? ReadonlyArray<WithoutIndices<U>>
-//                     : T[P] extends Primitive | Function
-//                         ? T[P]
-//                         : WithoutIndices<T[P]>
-//             : never
-// }
+// FIXME?
+type DeepClean<T> = Clean<{
+    [P in keyof T]: T[P] extends never
+        ? never
+        : T[P] extends Array<infer U>
+            ? Array<U>
+            : T[P] extends ReadonlyArray<infer U>
+                ? ReadonlyArray<U>
+                : T[P] extends Primitive | Function
+                    ? T[P]
+                    : DeepClean<T[P]>
+}>
 
-type WithoutIndices<T extends Record<any, any>> = Pick<T, KnownKeys<T>>
+let DC: DeepClean<{n: number, o: {b: boolean, n: never, o: {}}}> = {
+    n: 9,
+    o: {
+        b: true
+    }
+}
 
 type EventFunction = 'onabort' | 'onactive' | 'onaddtrack' | 'onafterprint' | 'onanimationcancel' | 'onanimationend' | 'onanimationiteration' | 'onanimationstart' | 'onaudioend' | 'onaudioprocess' | 'onaudiostart' | 'onauxclick' | 'onbeforeprint' | 'onbeforeunload' | 'onblocked' | 'onblur' | 'onbounce' | 'onboundary' | 'onbufferedamountlow' | 'oncached' | 'oncancel' | 'oncandidatewindowhide' | 'oncandidatewindowshow' | 'oncandidatewindowupdate' | 'oncanplay' | 'oncanplaythrough' | 'onchange' | 'onchecking' | 'onclick' | 'onclose' | 'oncompassneedscalibration' | 'oncomplete' | 'onconnectionstatechange' | 'oncontextmenu' | 'oncontrollerchange' | 'oncopy' | 'oncuechange' | 'oncut' | 'ondatachannel' | 'ondblclick' | 'ondevicechange' | 'ondevicelight' | 'ondevicemotion' | 'ondeviceorientation' | 'ondownloading' | 'ondrag' | 'ondragend' | 'ondragenter' | 'ondragexit' | 'ondragleave' | 'ondragover' | 'ondragstart' | 'ondrop' | 'ondurationchange' | 'onemptied' | 'onencrypted' | 'onend' | 'onended' | 'onenter' | 'onerror' | 'onexit' | 'onfinish' | 'onfocus' | 'onfullscreenchange' | 'onfullscreenerror' | 'ongatheringstatechange' | 'ongotpointercapture' | 'onhashchange' | 'onicecandidate' | 'onicecandidateerror' | 'oniceconnectionstatechange' | 'onicegatheringstatechange' | 'oninactive' | 'oninput' | 'oninvalid' | 'onisolationchange' | 'onkeydown' | 'onkeypress' | 'onkeyup' | 'onlanguagechange' | 'onload' | 'onloadeddata' | 'onloadedmetadata' | 'onloadend' | 'onloadstart' | 'onlocalcandidate' | 'onlostpointercapture' | 'onmark' | 'onmessage' | 'onmessageerror' | 'onmousedown' | 'onmouseenter' | 'onmouseleave' | 'onmousemove' | 'onmouseout' | 'onmouseover' | 'onmouseup' | 'onmousewheel' | 'onmsgesturechange' | 'onmsgesturedoubletap' | 'onmsgestureend' | 'onmsgesturehold' | 'onmsgesturestart' | 'onmsgesturetap' | 'onmsinertiastart' | 'onmsneedkey' | 'onmspointercancel' | 'onmspointerdown' | 'onmspointerenter' | 'onmspointerleave' | 'onmspointermove' | 'onmspointerout' | 'onmspointerover' | 'onmspointerup' | 'onMSVideoFormatChanged' | 'onMSVideoFrameStepCompleted' | 'onMSVideoOptimalLayoutChanged' | 'onmute' | 'onnegotiationneeded' | 'onnomatch' | 'onnoupdate' | 'onobsolete' | 'onoffline' | 'ononline' | 'onopen' | 'onorientationchange' | 'onoverconstrained' | 'onpagehide' | 'onpageshow' | 'onpaste' | 'onpause' | 'onplay' | 'onplaying' | 'onpointercancel' | 'onpointerdown' | 'onpointerenter' | 'onpointerleave' | 'onpointermove' | 'onpointerout' | 'onpointerover' | 'onpointerup' | 'onpopstate' | 'onprocessorerror' | 'onprogress' | 'onratechange' | 'onreadystatechange' | 'onrejectionhandled' | 'onremovetrack' | 'onreset' | 'onresize' | 'onresourcetimingbufferfull' | 'onresult' | 'onresume' | 'onscroll' | 'onsecuritypolicyviolation' | 'onseeked' | 'onseeking' | 'onselect' | 'onselectedcandidatepairchange' | 'onshippingaddresschange' | 'onshippingoptionchange' | 'onshow' | 'onsignalingstatechange' | 'onsoundend' | 'onsoundstart' | 'onspeechend' | 'onspeechstart' | 'onstalled' | 'onstart' | 'onstatechange' | 'onstatsended' | 'onstorage' | 'onsubmit' | 'onsuccess' | 'onsuspend' | 'ontimeout' | 'ontimeupdate' | 'ontoggle' | 'ontonechange' | 'ontouchcancel' | 'ontouchend' | 'ontouchmove' | 'ontouchstart' | 'ontrack' | 'ontransitioncancel' | 'ontransitionend' | 'ontransitionrun' | 'ontransitionstart' | 'onunhandledrejection' | 'onunload' | 'onunmute' | 'onupdatefound' | 'onupdateready' | 'onupgradeneeded' | 'onversionchange' | 'onvisibilitychange' | 'onvoiceschanged' | 'onvolumechange' | 'onvrdisplayactivate' | 'onvrdisplayblur' | 'onvrdisplayconnect' | 'onvrdisplaydeactivate' | 'onvrdisplaydisconnect' | 'onvrdisplayfocus' | 'onvrdisplaypointerrestricted' | 'onvrdisplaypointerunrestricted' | 'onvrdisplaypresentchange' | 'onwaiting' | 'onwheel' | 'onzoom'
 
-type WithoutFunctions<T> = {
-    [P in keyof T]: T[P] extends never
-        ? never
-        : T[P] extends Array<infer U>
-            ? Array<WithoutFunctions<U>>
-            : T[P] extends ReadonlyArray<infer U>
-                ? ReadonlyArray<WithoutFunctions<U>>
-                : T[P] extends Function | Nil
-                    ? P extends EventFunction
-                        ? T[P]
-                        : never
-                    : T[P] extends Primitive
-                        ? T[P]
-                        : WithoutFunctions<T[P]>
+type Props<E extends Element> = Clean<DeepProps<E>>
+
+type DeepProps<T> = {
+    [P in keyof T]?: T[P] extends Array<infer U>
+        ? U extends Primitive | Function
+            ? Array<U>
+            : Array<DeepProps<U>>
+        : T[P] extends ReadonlyArray<infer U>
+            ? U extends Primitive | Function
+                ? ReadonlyArray<U>
+                : ReadonlyArray<DeepProps<U>>
+            : T[P] extends Function
+                ? P extends EventFunction
+                    ? IfEquals<{[_ in P]: T[P]}, {-readonly [_ in P]: T[P]}, T[P], never>
+                    : never
+                : T[P] extends Primitive
+                    ? IfEquals<{[_ in P]: T[P]}, {-readonly [_ in P]: T[P]}, T[P], never>
+                    : DeepProps<T>
 }
 
-type WithoutReadonly<T> = {
-    [P in keyof T]: T[P] extends never
-        ? never
-        : T[P] extends Primitive | Function
-            ? IfEquals<{[_ in P]: T[P]}, {-readonly [_ in P]: T[P]}, T[P], never>
-            : T[P] extends Array<infer U>
-                ? Array<WithoutReadonly<U>>
-                : T[P] extends ReadonlyArray<infer U>
-                    ? ReadonlyArray<WithoutReadonly<U>>
-                    : WithoutReadonly<T[P]>
-}
+type FormProps = Props<HTMLFormElement>
 
-type ElementOptions<E extends Element> =
-    PickDefined<
-        DeepPartial<
-            WithoutIndices<
-                WithoutFunctions<
-                    WithoutReadonly<
-                        E>>>>>
-
-function form(_props: ElementOptions<HTMLFormElement>): HTMLFormElement {
+function form(_props: FormProps): HTMLFormElement {
     return document.createElement('form')
 }
 
-// TODO Void elements.
-
 form({
-    // ooooooo: 'evt',
+    ooooooo: undefined,
+    name: 'myForm',
     onclick: (evt: MouseEvent) => console.log('WAT?', evt),
     style: {
         display: 'block',
-        // parentRule: {},
-        99: '99'
+        parentRule: {},
+        asdf: '99'
     }
 })
