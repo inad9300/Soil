bd952c03f5bbbf70bf6bb60c73d87c9909c5251a
Daniel M <inad9300@gmail.com>
2018-12-20 14:47:56 +0100

    Update $.ts

--- dom/src/$.ts
+++ dom/src/$.ts
@@ -1,4 +1,20 @@
 /**
- * Alias of `document.querySelector()`, for the sake of conciseness.
+ * Function for DOM element selection, similar to the native `ParentNode.querySelector()`. It takes
+ * advantage of the "universal" understanding of `$` as a selector function for maximum conciseness.
+ *
+ * TODO Improve function signatures.
  */
-export const $ = document.querySelector
+export function $(selector: string): Element | null
+export function $(element: Element, selector: string): Element | null
+export function $(selectorOrElement: string | Element, nothingOrSelector: void | string): Element | null {
+    let context: Element
+    let selector: string
+    if (typeof selectorOrElement === 'string') {
+        context = document
+        selector = selectorOrElement
+    } else {
+        context = selectorOrElement
+        selector = nothingOrSelector
+    }
+    return context.querySelector(selector)
+}
