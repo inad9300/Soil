3efe902c6cc9b29086026f5a5336879b6ba0acef
Daniel M <inad9300@gmail.com>
2018-12-31 16:12:46 +0100

    Add optional context argument to $$

--- dom/src/$$.ts
+++ dom/src/$$.ts
@@ -5,9 +5,24 @@
  *
  * The function signatures are derived from the native interface `NodeSelector`.
  */
-export function $$<K extends keyof HTMLElementTagNameMap>(selectors: K): HTMLElementTagNameMap[K][]
-export function $$<K extends keyof SVGElementTagNameMap>(selectors: K): SVGElementTagNameMap[K][]
-export function $$<E extends Element = Element>(selectors: string): E[]
-export function $$(selectors: string) {
-    return Array.from(document.querySelectorAll(selectors))
+export function $$<K extends keyof HTMLElementTagNameMap>(selector: K): HTMLElementTagNameMap[K][]
+export function $$<K extends keyof HTMLElementTagNameMap>(element: Element, selector: K): HTMLElementTagNameMap[K][]
+
+export function $$<K extends keyof SVGElementTagNameMap>(selector: K): SVGElementTagNameMap[K][]
+export function $$<K extends keyof SVGElementTagNameMap>(element: Element, selector: K): SVGElementTagNameMap[K][]
+
+export function $$<E extends Element = Element>(selector: string): E[]
+export function $$<E extends Element = Element>(element: Element, selector: string): E[]
+
+export function $$(selectorOrElement: string | Element, nothingOrSelector: void | string) {
+    let context: Element
+    let selector: string
+    if (typeof selectorOrElement === 'string') {
+        context = document
+        selector = selectorOrElement
+    } else {
+        context = selectorOrElement
+        selector = nothingOrSelector
+    }
+    return Array.from(context.querySelectorAll(selector))
 }
