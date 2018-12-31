28392b5caa5b3c89f32d9ff7c5ee63c3ac5af9cb
Daniel M <inad9300@gmail.com>
2018-12-31 16:18:08 +0100

    Improve signatures of $

--- dom/src/$.ts
+++ dom/src/$.ts
@@ -1,11 +1,16 @@
 /**
  * Function for DOM element selection, similar to the native `ParentNode.querySelector()`. It takes
  * advantage of the "universal" understanding of `$` as a selector function for maximum conciseness.
- *
- * TODO Improve function signatures.
  */
-export function $(selector: string): Element | null
-export function $(element: Element, selector: string): Element | null
+export function $<K extends keyof HTMLElementTagNameMap>(selector: K): HTMLElementTagNameMap[K] | null
+export function $<K extends keyof HTMLElementTagNameMap>(element: Element, selector: K): HTMLElementTagNameMap[K] | null
+
+export function $<K extends keyof SVGElementTagNameMap>(selector: K): SVGElementTagNameMap[K] | null
+export function $<K extends keyof SVGElementTagNameMap>(element: Element, selector: K): SVGElementTagNameMap[K] | null
+
+export function $<E extends Element = Element>(selector: string): E | null
+export function $<E extends Element = Element>(element: Element, selector: string): E | null
+
 export function $(selectorOrElement: string | Element, nothingOrSelector: void | string): Element | null {
     let context: Element
     let selector: string
