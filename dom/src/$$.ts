73db60cd85b54aee8b7f6defa130ef61843aade0
inad9300 <inad9300@gmail.com>
2019-02-10 15:59:25 +0100

    Simplify 'dollar' functions

--- dom/src/$$.ts
+++ dom/src/$$.ts
@@ -1,27 +1,12 @@
 /**
  * Enhanced alias of `document.querySelectorAll()`, for the sake of conciseness.
  * In contrast with the native function, the return type is `Array` and not
- * `NodeListOf`, and an optional element can be provided as first parameter to
- * determine the selection context.
+ * `NodeListOf`, and an optional element can be provided to determine the
+ * selection context.
  */
-export function $$<K extends keyof HTMLElementTagNameMap>(selector: K): HTMLElementTagNameMap[K][]
-export function $$<K extends keyof HTMLElementTagNameMap>(element: Element, selector: K): HTMLElementTagNameMap[K][]
-
-export function $$<K extends keyof SVGElementTagNameMap>(selector: K): SVGElementTagNameMap[K][]
-export function $$<K extends keyof SVGElementTagNameMap>(element: Element, selector: K): SVGElementTagNameMap[K][]
-
-export function $$<E extends Element = Element>(selector: string): E[]
-export function $$<E extends Element = Element>(element: Element, selector: string): E[]
-
-export function $$(selectorOrElement: string | Element, nothingOrSelector: void | string) {
-    let context: Element
-    let selector: string
-    if (typeof selectorOrElement === 'string') {
-        context = document
-        selector = selectorOrElement
-    } else {
-        context = selectorOrElement
-        selector = nothingOrSelector
-    }
+export function $$<K extends keyof HTMLElementTagNameMap>(selector: K, context?: ParentNode): HTMLElementTagNameMap[K][]
+export function $$<K extends keyof SVGElementTagNameMap>(selector: K, context?: ParentNode): SVGElementTagNameMap[K][]
+export function $$<E extends Element = Element>(selector: string, context?: ParentNode): E[]
+export function $$(selector: string, context: ParentNode = document): Element[] {
     return Array.from(context.querySelectorAll(selector))
 }
--- dom/src/$.ts
+++ dom/src/$.ts
@@ -1,26 +1,11 @@
 /**
  * Function for DOM element selection, similar to the native `ParentNode.querySelector()`. It takes
  * advantage of the "universal" understanding of `$` as a selector function for maximum conciseness.
- * An optional element can be provided as first parameter to determine the selection context.
+ * An optional element can be provided to determine the selection context.
  */
-export function $<K extends keyof HTMLElementTagNameMap>(selector: K): HTMLElementTagNameMap[K] | null
-export function $<K extends keyof HTMLElementTagNameMap>(element: Element, selector: K): HTMLElementTagNameMap[K] | null
-
-export function $<K extends keyof SVGElementTagNameMap>(selector: K): SVGElementTagNameMap[K] | null
-export function $<K extends keyof SVGElementTagNameMap>(element: Element, selector: K): SVGElementTagNameMap[K] | null
-
-export function $<E extends Element = Element>(selector: string): E | null
-export function $<E extends Element = Element>(element: Element, selector: string): E | null
-
-export function $(selectorOrElement: string | Element, nothingOrSelector: void | string): Element | null {
-    let context: Element
-    let selector: string
-    if (typeof selectorOrElement === 'string') {
-        context = document
-        selector = selectorOrElement
-    } else {
-        context = selectorOrElement
-        selector = nothingOrSelector
-    }
+export function $<K extends keyof HTMLElementTagNameMap>(selector: K, context?: ParentNode): HTMLElementTagNameMap[K] | null
+export function $<K extends keyof SVGElementTagNameMap>(selector: K, context?: ParentNode): SVGElementTagNameMap[K] | null
+export function $<E extends Element = Element>(selector: string, context?: ParentNode): E | null
+export function $(selector: string, context: ParentNode = document): Element | null {
     return context.querySelector(selector)
 }
